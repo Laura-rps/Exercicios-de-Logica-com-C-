29f5dce25360aeea5b9146e4be0feb22eceacef9
Laura Roberto <lauraroberto731@gmail.com>
2025-06-04 12:54:33 -0300

    04/06

---
+++ TesteSubString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosLogica
+{
+    internal class TesteSubString
+    {
+        public static void Main(string[] args)
+        {
+            string nome = "Laura Roberto";
+            int charPos = nome.IndexOf("R");
+            string sobrenome = nome.Substring(charPos);
+            Console.WriteLine(sobrenome);
+        }
+    }
+}
